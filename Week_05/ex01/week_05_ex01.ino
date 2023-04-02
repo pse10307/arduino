e1648b8a3fcaac2b8ea597171012e6a62341074e
pse10307 <109071820+pse10307@users.noreply.github.com>
2023-04-02 20:20:49 +0900

    Add files via upload

---
+++ Week_05/ex01/week_05_ex01.ino
@@ -0,0 +1,16 @@
+// TMP36 온도 측정
+int Pin = A0;   // 입력 핀
+int reading; 
+float tempC;
+float V;
+void setup() {
+  Serial.begin(9600);   // 시리얼 통신 속도 설정
+}
+void loop() {
+  reading = analogRead(Pin);   // 센서 값 읽어옴
+  V=reading*5.0/1023;
+  tempC=(V-0.5)*100;
+  Serial.print(tempC);   // 센서 값 섭씨온도로 변환(TMP36 계산 공식에 의함)
+  Serial.println(" 도");
+  delay(1000);   // 온도 측정 간격 설정 (1000 = 1초)
+}
